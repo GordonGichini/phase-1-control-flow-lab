39d79223b39f6f035ac0cad0c801fd3281285177
GordonGichini <gordongichini1@gmail.com>
2022-06-20 01:03:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+if('ride <= 400feet')
+ return 'This one is on me!'
+}
+ // Write your code here!
+else if (ride > 2000 && <= 2500){
+console.log ('I will gladly take your 30 bucks.')
+}
+else if('ride >= 2500ft'){
+console.log ('No can do');
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.':'No go.'
+  //if(city=='NYC'){
+ //   return 'Ok,sounds good.'
+//}else{
+//  return 'No go.'
+//}
+  
+
+  }
   // Write your code here!
-}
+
 
 function switchOnCharmFromTip(){
+  //if(str === 'generous'){
+    return 'Thank you so ch'
+  }
   // Write your code here!
 }
